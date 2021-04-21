af483df6aa4b195fa1729ad06bc3debb8ea979fc
Henno Täht <henno.taht@gmail.com>
2021-04-21 14:54:14 +0300

    Improve FinvoiceSettings

--- tests/test.php
+++ tests/test.php
@@ -1,8 +1,55 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+use henno\Finvoice\Finvoice;
+use henno\Finvoice\FinvoiceSettings;
 
-$settings = new Finvoice\FinvoiceSettings();
-$finvoice = new Finvoice\Finvoice($settings);
+require_once __DIR__ . '/../vendor/autoload.php';
 
-echo $finvoice;
+$finvoice = new Finvoice(new FinvoiceSettings([
+        'from' => [
+            'IBAN' => 'FI2487000710446218',
+            'BIC' => 'DABAFIHH',
+            'name' => 'Virtue Softworks',
+            'business_id' => '2444711-4',
+            'address' => 'Minna Canthin katu 4 A 4. krs',
+            'postcode' => '70200',
+            'city' => 'Kuopio'
+        ],
+        'to' => [
+            'IBAN' => 'FI3387000710510658',
+            'BIC' => 'DABAFIHH',
+            'name' => 'OmaStore Osuuskunta',
+            'business_id' => '2527031-4',
+            'address' => 'Minna Canthin katu 4 A 4. krs',
+            'postcode' => '70200',
+            'city' => 'Kuopio'
+        ],
+        'invoice' => [
+            'id' => 275536,
+            'order_id' => 100025,
+            'date' => '2013-07-02',
+            'due_date' => '2013-07-15',
+            'reference_number' => 2755366,
+            'rows' => [
+                (object)[
+                    'id' => 1331,
+                    'name' => 'Tuntityö',
+                    'amount' => 5.5,
+                    'unit' => 'h',
+                    'price' => 50,
+                    'vat' => 24
+                ],
+                (object)[
+                    'id' => 1332,
+                    'name' => 'Verkkotunnuksen rekisteröinti',
+                    'amount' => 1,
+                    'unit' => 'kpl',
+                    'price' => 20,
+                    'vat' => 24
+                ]
+            ]
+        ]
+    ]
+), false);
+
+echo $finvoice->getXML();
