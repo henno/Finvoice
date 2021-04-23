95e794c06db69d66a0a5a83f850dac0f3e20b32b
oliverkill <killoliver08@gmail.com>
2021-04-23 17:40:03 +0300

    Scoro compability

--- src/Finvoice.php
+++ src/Finvoice.php
@@ -74,7 +74,7 @@ class Finvoice
                     'id' => (string)$finvoice->EpiDetails->EpiPaymentInstructionDetails->EpiPaymentInstructionId,
                     'date' => date('Y-m-d', strtotime($finvoice->EpiDetails->EpiIdentificationDetails->EpiDate)),
                     'due_date' => date('Y-m-d', strtotime($finvoice->EpiDetails->EpiPaymentInstructionDetails->EpiDateOptionDate)),
-                    'reference_number' => (string)$finvoice->EpiDetails->EpiPaymentInstructionDetails->EpiRemittanceInfoIdentifier,
+                    'epi_reference' => (string)$finvoice->EpiDetails->EpiPaymentInstructionDetails->EpiRemittanceInfoIdentifier,
                     'amount' => self::float((string)$finvoice->EpiDetails->EpiPaymentInstructionDetails->EpiInstructedAmount),
                     'rows' => []
                 ];
@@ -219,18 +219,20 @@ class Finvoice
     public function getFinvoice()
     {
         $finvoice = new \SimpleXMLElement('<Finvoice/>');
-        $finvoice->addAttribute('Version', '2.0');
+        $finvoice->addAttribute('Version', '3.0');
         $finvoice->addAttribute('xsi:noNamespaceSchemaLocation', 'Finvoice.xsd', $this->namespaces['xsi']);
 
         $messageTransmissionDetails = $finvoice->addChild('MessageTransmissionDetails');
 
         $messageSenderDetails = $messageTransmissionDetails->addChild('MessageSenderDetails');
-        $messageSenderDetails->addChild('FromIdentifier', $this->settings->from->IBAN);
-        $messageSenderDetails->addChild('FromIntermediator', $this->settings->from->BIC);
+        $messageSenderDetails->addChild('FromIdentifier', $this->settings->from->identifier)->addAttribute('SchemeID', $this->settings->from->identifier_scheme_id);
+        $messageSenderDetails->addChild('FromIntermediator', $this->settings->from->intermediator);
 
         $messageReceiverDetails = $messageTransmissionDetails->addChild('MessageReceiverDetails');
-        $messageReceiverDetails->addChild('ToIdentifier', $this->settings->to->IBAN);
-        $messageReceiverDetails->addChild('ToIntermediator', $this->settings->to->BIC);
+        $messageReceiverDetails->addChild('ToIdentifier', $this->settings->to->identifier)->addAttribute('SchemeID', $this->settings->to->identifier_scheme_id);
+        if (isset($this->settings->to->intermediator)) {
+            $messageReceiverDetails->addChild('ToIntermediator', $this->settings->to->intermediator);
+        }
 
         $messageDetails = $messageTransmissionDetails->addChild('MessageDetails');
         $messageDetails->addChild('MessageIdentifier', $this->id);
@@ -239,19 +241,21 @@ class Finvoice
         $sellerPartyDetails = $finvoice->addChild('SellerPartyDetails');
         $sellerPartyDetails->addChild('SellerPartyIdentifier', $this->settings->from->business_id);
         $sellerPartyDetails->addChild('SellerOrganisationName', $this->settings->from->name);
+        $sellerPartyDetails->addChild('SellerOrganisationTaxCode', $this->settings->from->tax_code);
         $sellerPostalAddressDetails = $sellerPartyDetails->addChild('SellerPostalAddressDetails');
 
         $sellerPostalAddressDetails->addChild('SellerStreetName', $this->settings->from->address);
         $sellerPostalAddressDetails->addChild('SellerTownName', $this->settings->from->city);
         $sellerPostalAddressDetails->addChild('SellerPostCodeIdentifier', $this->settings->from->postcode);
         $sellerPostalAddressDetails->addChild('CountryCode', 'FI');
+        $sellerPostalAddressDetails->addChild('CountryName', 'Suomi');
 
-        $finvoice->addChild('SellerOrganisationUnitNumber', '0037' . str_replace('-', '', $this->settings->from->business_id) . '000001');
-        $finvoice->addChild('SellerContactPersonName', isset($this->settings->from->contact) ? $this->settings->from->contact : null);
+        #$finvoice->addChild('SellerOrganisationUnitNumber', '0037' . str_replace('-', '', $this->settings->from->business_id));
+        #$finvoice->addChild('SellerContactPersonName', isset($this->settings->from->contact) ? $this->settings->from->contact : null);
 
-        $sellerCommunicationDetails = $finvoice->addChild('SellerCommunicationDetails');
-        $sellerCommunicationDetails->addChild('SellerPhoneNumberIdentifier', isset($this->settings->from->phone) ? $this->settings->from->phone : null);
-        $sellerCommunicationDetails->addChild('SellerEmailaddressIdentifier', isset($this->settings->from->email) ? $this->settings->from->email : null);
+        #$sellerCommunicationDetails = $finvoice->addChild('SellerCommunicationDetails');
+        #$sellerCommunicationDetails->addChild('SellerPhoneNumberIdentifier', isset($this->settings->from->phone) ? $this->settings->from->phone : null);
+        #$sellerCommunicationDetails->addChild('SellerEmailaddressIdentifier', isset($this->settings->from->email) ? $this->settings->from->email : null);
 
         $sellerInformationDetails = $finvoice->addChild('SellerInformationDetails');
         $sellerAccountDetails = $sellerInformationDetails->addChild('SellerAccountDetails');
@@ -288,12 +292,15 @@ class Finvoice
 
         $invoiceDetails = $finvoice->addChild('InvoiceDetails');
         $invoiceDetails->addChild('InvoiceTypeCode', 'INV01')->addAttribute('CodeListAgencyIdentifier', 'SPY');
-        $invoiceDetails->addChild('InvoiceTypeText', 'LASKU');
+        $invoiceDetails->addChild('InvoiceTypeText', 'INVOICE');
         $invoiceDetails->addChild('OriginCode', 'Original');
-        $invoiceDetails->addChild('InvoiceNumber', $this->settings->invoice->id);
+        $invoiceDetails->addChild('InvoiceNumber', $this->settings->invoice->no);
         $invoiceDetails->addChild('InvoiceDate', date('Ymd', strtotime($this->settings->invoice->date)))->addAttribute('Format', 'CCYYMMDD');
-        $invoiceDetails->addChild('OrderIdentifier', $this->settings->invoice->order_id);
-        $invoiceDetails->addChild('BuyerReferenceIdentifier', isset($this->settings->invoice->buyer_reference) ? $this->settings->invoice->buyer_reference : null);
+        if (isset($this->settings->invoice->order_no)) {
+            $invoiceDetails->addChild('OrderIdentifier', $this->settings->invoice->order_no);
+        }
+        $invoiceDetails->addChild('BuyerReferenceIdentifier', isset($this->settings->invoice->description) ? $this->settings->invoice->description : null);
+        $invoiceDetails->addChild('SellerReferenceIdentifier', isset($this->settings->invoice->reference_no) ? $this->settings->invoice->reference_no : null);
 
         $totalVatExcludedAmount = 0;
         $totalVatAmount = 0;
@@ -301,59 +308,49 @@ class Finvoice
 
         $vat = [];
 
-        foreach ($this->settings->invoice->rows as $row) {
+        foreach ($this->settings->invoice->lines as $row) {
             $row = (object)$row;
             $vatExcludedAmount = $row->amount * $row->price;
-            $vatAmount = $row->price * ($row->vat / 100) * $row->amount;
+            $vatAmount = $row->price * ($this->settings->invoice->vat / 100) * $row->amount;
             $vatIncludedAmount = $vatExcludedAmount + $vatAmount;
 
-            if (!isset($vat[$row->vat])) {
-                $vat[$row->vat] = (object)[
-                    'vatBaseAmount' => 0,
-                    'vatRatePercent' => $row->vat,
-                    'vatRateAmount' => 0
-                ];
-            }
-
-            $vat[$row->vat]->vatBaseAmount += $vatExcludedAmount;
-            $vat[$row->vat]->vatRateAmount += $vatAmount;
-
-            $totalVatExcludedAmount += $vatExcludedAmount;
-            $totalVatAmount += $vatAmount;
-            $totalVatIncludedAmount += $vatIncludedAmount;
         }
 
-        $invoiceDetails->addChild('InvoiceTotalVatExcludedAmount', number_format($totalVatExcludedAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
-        $invoiceDetails->addChild('InvoiceTotalVatAmount', number_format($totalVatAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
-        $invoiceDetails->addChild('InvoiceTotalVatIncludedAmount', number_format($totalVatIncludedAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
+        $invoiceDetails->addChild('InvoiceTotalVatExcludedAmount', number_format($this->settings->invoice->sum, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
+        $invoiceDetails->addChild('InvoiceTotalVatAmount', number_format($this->settings->invoice->vat_sum, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
+        $invoiceDetails->addChild('InvoiceTotalVatIncludedAmount', number_format($this->settings->invoice->vat_sum + $this->settings->invoice->sum, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
         foreach ($vat as $vatInfo) {
             $vatSpecificationDetails = $invoiceDetails->addChild('VatSpecificationDetails');
-            $vatSpecificationDetails->addChild('VatBaseAmount', number_format($vatInfo->vatBaseAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
+            $vatSpecificationDetails->addChild('VatBaseAmount', number_format($vatInfo->vatBaseAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
             $vatSpecificationDetails->addChild('VatRatePercent', $vatInfo->vatRatePercent);
-            $vatSpecificationDetails->addChild('VatRateAmount', number_format($vatInfo->vatRateAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
+            $vatSpecificationDetails->addChild('VatRateAmount', number_format($vatInfo->vatRateAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
         }
-        $invoiceDetails->addChild('PaymentTermsDetails')->addChild('InvoiceDueDate', date('Ymd', strtotime($this->settings->invoice->due_date)))->addAttribute('Format', 'CCYYMMDD');
+        $paymentTermsDetails = $invoiceDetails->addChild('PaymentTermsDetails');
+        $invoiceDueDate = $paymentTermsDetails->addChild('InvoiceDueDate', date('Ymd', strtotime($this->settings->invoice->deadline)))->addAttribute('Format', 'CCYYMMDD');
+        $paymentOverDueFineDetails = $paymentTermsDetails->addChild('PaymentOverDueFineDetails', null);
+        $paymentOverDueFineDetails->addChild('PaymentOverDueFinePercent', $this->settings->invoice->fine);
 
-        foreach ($this->settings->invoice->rows as $row) {
+        foreach ($this->settings->invoice->lines as $row) {
+            $row = (object)$row;
 
             $invoiceRow = $finvoice->addChild('InvoiceRow');
 
-            $invoiceRow->addChild('ArticleName', $row->name);
+            $invoiceRow->addChild('ArticleName', $row->product_name);
             if (isset($row->ordered)) {
                 $invoiceRow->addChild('OrderedQuantity', number_format($row->ordered, 2, ',', ''))->addAttribute('QuantityUnitCode', $row->unit);
             }
             $invoiceRow->addChild('InvoicedQuantity', number_format($row->amount, 2, ',', ''))->addAttribute('QuantityUnitCode', $row->unit);
             $unitPriceAmount = $invoiceRow->addChild('UnitPriceAmount', number_format($row->price, 2, ',', ''));
-            $unitPriceAmount->addAttribute('AmountCurrencyIdentifier', 'EUR');
+            $unitPriceAmount->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
             $unitPriceAmount->addAttribute('UnitPriceUnitCode', 'e/' . $row->unit);
-            $invoiceRow->addChild('UnitPriceVatIncludedAmount', number_format($row->price * (100 + $row->vat) / 100, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
+            $invoiceRow->addChild('UnitPriceVatIncludedAmount', number_format($row->price * (100 + $this->settings->invoice->vat) / 100, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
             if ($row->id > 0) {
                 $invoiceRow->addChild('RowIdentifier', $row->id);
             }
-            $invoiceRow->addChild('RowVatRatePercent', $row->vat);
-            $invoiceRow->addChild('RowVatAmount', number_format($row->amount * round($row->price * ($row->vat / 100), 2), 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
-            $invoiceRow->addChild('RowVatExcludedAmount', number_format($row->amount * $row->price, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
-            $invoiceRow->addChild('RowAmount', number_format($row->amount * round($row->price * (100 + $row->vat) / 100, 2), 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
+            $invoiceRow->addChild('RowVatRatePercent', $this->settings->invoice->vat);
+            $invoiceRow->addChild('RowVatAmount', number_format($row->amount * round($row->price * ($this->settings->invoice->vat / 100), 2), 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
+            $invoiceRow->addChild('RowVatExcludedAmount', number_format($row->amount * $row->price, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
+            $invoiceRow->addChild('RowAmount', number_format($row->amount * round($row->price * (100 + $this->settings->invoice->vat) / 100, 2), 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
         }
 
         #$specificationDetails = $finvoice->addChild('SpecificationDetails');
@@ -362,7 +359,7 @@ class Finvoice
         $epiDetails = $finvoice->addChild('EpiDetails');
         $epiIdentificationDetails = $epiDetails->addChild('EpiIdentificationDetails');
         $epiIdentificationDetails->addChild('EpiDate', date('Ymd', strtotime($this->settings->invoice->date)))->addAttribute('Format', 'CCYYMMDD');
-        $epiIdentificationDetails->addChild('EpiReference', $this->settings->invoice->reference_number);
+        $epiIdentificationDetails->addChild('EpiReference', isset($this->settings->invoice->epi_reference) ? $this->settings->invoice->epi_reference : null);
 
         $epiPartyDetails = $epiDetails->addChild('EpiPartyDetails');
         $epiBfiPartyDetails = $epiPartyDetails->addChild('EpiBfiPartyDetails');
@@ -373,10 +370,10 @@ class Finvoice
         $epiBeneficiaryPartyDetails->addChild('EpiAccountID', $this->settings->from->IBAN)->addAttribute('IdentificationSchemeName', 'IBAN');
 
         $epiPaymentInstructionDetails = $epiDetails->addChild('EpiPaymentInstructionDetails');
-        $epiPaymentInstructionDetails->addChild('EpiRemittanceInfoIdentifier', $this->settings->invoice->reference_number)->addAttribute('IdentificationSchemeName', 'SPY');
-        $epiPaymentInstructionDetails->addChild('EpiInstructedAmount', number_format($totalVatIncludedAmount, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', 'EUR');
-        $epiPaymentInstructionDetails->addChild('EpiCharge', 'SHA')->addAttribute('ChargeOption', 'SHA');
-        $epiPaymentInstructionDetails->addChild('EpiDateOptionDate', date('Ymd', strtotime($this->settings->invoice->due_date)))->addAttribute('Format', 'CCYYMMDD');
+        #$epiPaymentInstructionDetails->addChild('EpiRemittanceInfoIdentifier', null)->addAttribute('IdentificationSchemeName', $this->settings->invoice->epi_remittance_info_identifier_identification_scheme_name);
+        $epiPaymentInstructionDetails->addChild('EpiInstructedAmount', number_format($this->settings->invoice->vat_sum + $this->settings->invoice->sum, 2, ',', ''))->addAttribute('AmountCurrencyIdentifier', $this->settings->invoice->currency);
+        $epiPaymentInstructionDetails->addChild('EpiCharge', null)->addAttribute('ChargeOption', 'SHA');
+        $epiPaymentInstructionDetails->addChild('EpiDateOptionDate', date('Ymd', strtotime($this->settings->invoice->deadline)))->addAttribute('Format', 'CCYYMMDD');
 
         return $finvoice;
     }
